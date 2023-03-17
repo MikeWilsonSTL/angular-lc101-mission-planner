21cf9938d110fb4f59004c528212e9a6a02e76ea
Mike Wilson <mikewilsonstl@gmail.com>
2023-03-16 19:46:13 -0500

    Completed Studio Part 3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,34 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
-  memberBeingEdited: object = null;
-  crew: object[] = [
-    { name: "Eileen Collins", firstMission: false },
-    { name: "Mae Jemison", firstMission: false },
-    { name: "Ellen Ochoa", firstMission: true }
+  inCrew: boolean = false;
+  crew: object[] = [];
+
+  candidates: object[] = [
+    { name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg' },
+    { name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg' },
+    { name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg' },
+    { name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg' },
+    { name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg' },
+    { name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg' },
+    { name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg' }
   ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
-  add(memberName: string, isFirst: boolean) {
-    this.crew.push({ name: memberName, firstMission: isFirst });
+  // Code the 'addCrewMember' function here:
+  addCrewMember(candidate: object) {
+    // if (!this.crew.includes(candidate) && this.crew.length < 3) {
+    this.crew.push(candidate);
+    // }
+    // else {
+    //   this.crew.splice(this.crew.indexOf(candidate), 1);
+    // }
   }
-
-  remove(member: object) {
-    let index = this.crew.indexOf(member);
-    this.crew.splice(index, 1);
-  }
-
-  edit(member: object) {
-    this.memberBeingEdited = member;
-  }
-
-  save(name: string, member: object) {
-    member['name'] = name;
-    this.memberBeingEdited = null;
-  }
-
 }
